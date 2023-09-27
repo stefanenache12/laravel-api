a410477c7750d0a2001d0d3dff29f60d06af2ef1
stefanenache12 <stefanenache12@gmail.com>
2023-09-27 16:58:17 +0200

    Add API Controller

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -13,6 +13,7 @@ class ProjectController extends Controller
     {
         $projects = Project::all();
         
+
         return response()->json([
             'success' => true,
             'results' => $projects
