3aa5fe8f6dc0f154c7fa3a6fa5bb50bd97b655df
stefanenache12 <stefanenache12@gmail.com>
2023-09-27 17:21:53 +0200

    Add project relations

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -11,9 +11,8 @@ class ProjectController extends Controller
 {
     public function index()
     {
-        $projects = Project::all();
+        $projects = Project::with('type','technologies')->paginate(5);
         
-
         return response()->json([
             'success' => true,
             'results' => $projects
